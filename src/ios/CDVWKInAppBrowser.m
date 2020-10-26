d52c7f1b3926fa76dcb395131758b5abebf1da78
Greg Tzar <livingegg@gmail.com>
2020-10-26 10:10:13 -0700

    Applied the issue-592 fix to support tel and mailto links for WKWebView. This branch originated from the latest version 4.0.0 tag and new version tags can be merged from the source repo into this branch to keep it up to date as needed until the fix is applied in the source repo and this branch can be dropped.

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -538,7 +538,8 @@ static CDVWKInAppBrowser* instance = nil;
     }
     
     //if is an app store link, let the system handle it, otherwise it fails to load it
-    if ([[ url scheme] isEqualToString:@"itms-appss"] || [[ url scheme] isEqualToString:@"itms-apps"]) {
+    //NOTE: The following condition has be modified to reflect the WKWebView fixes for tel and mailto links as noted in this issue: https://github.com/apache/cordova-plugin-inappbrowser/issues/592
+    if ([[ url scheme] isEqualToString:@"itms-appss"] || [[ url scheme] isEqualToString:@"itms-apps"] || [[ url scheme] isEqualToString:@"tel"] || [[ url scheme] isEqualToString:@"sms"] || [[ url scheme] isEqualToString:@"mailto"] || [[ url scheme] isEqualToString:@"geo"]) {
         [theWebView stopLoading];
         [self openInSystem:url];
         shouldStart = NO;
